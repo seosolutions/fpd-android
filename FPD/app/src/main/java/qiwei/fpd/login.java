41a0a9b3a50517ecc304c05de50df523453577bf
Qiwei <liqiwei1224@gmail.com>
2015-06-07 21:15:29 -0700

    login page database works

--- FPD/app/src/main/java/qiwei/fpd/login.java
+++ FPD/app/src/main/java/qiwei/fpd/login.java
@@ -1,5 +1,11 @@
 package qiwei.fpd;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import android.os.AsyncTask;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -19,6 +25,8 @@ public class login extends ActionBarActivity {
     private static Button login_btn;
     private static Button create_account_btn;
     int attempt_counter = 3;
+    private static String mastername;
+    private static String masterpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,34 +37,71 @@ public class login extends ActionBarActivity {
     }
 
 
+    private class FetchSQLgetpassword extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            String retval = "";
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+                retval = e.toString();
+            }
+            String url = "jdbc:postgresql://10.0.2.2/postgres?user=postgres&password=1234";
+            Connection conn;
+            try {
+                DriverManager.setLoginTimeout(5);
+                conn = DriverManager.getConnection(url);
+                Statement st = conn.createStatement();
+                String sql;
+                sql = "SELECT * FROM \"userTable\" WHERE username = '" + mastername +"';";
+                ResultSet rs = st.executeQuery(sql);
+                while(rs.next()) {
+                    retval = rs.getString(2);
+                }
+                rs.close();
+                st.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                retval = e.toString();
+            }
+            return retval;
+        }
+        @Override
+        protected void onPostExecute(String value) {
+            if (masterpassword.equals(value)) {
+                Toast.makeText(login.this, "Username and password are correct",
+                        Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent("qiwei.fpd.user");
+                startActivity(intent);
+            } else {
+                Toast.makeText(login.this, "Username or password is not correct",
+                        Toast.LENGTH_SHORT).show();
+                attempt_counter--;
+                attempts.setText(Integer.toString(attempt_counter));
+                if(attempt_counter == 0){
+                    login_btn.setEnabled(false);
+                }
+            }
+        }
+    }
+
     public void LoginButton(){
         username = (EditText)findViewById(R.id.editText_username);
         password = (EditText)findViewById(R.id.editText_password);
         attempts = (TextView)findViewById(R.id.textView_attempts_counter);
         login_btn = (Button)findViewById(R.id.button_login);
-
         attempts.setText(Integer.toString(attempt_counter));
 
         login_btn.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (username.getText().toString().equals("qiwei") &&
-                                password.getText().toString().equals("qiwei66")) {
-                            Toast.makeText(login.this, "Username and password are correct",
-                                    Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent("qiwei.fpd.user");
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(login.this, "Username or password is not correct",
-                                    Toast.LENGTH_SHORT).show();
-                            attempt_counter--;
-                            attempts.setText(Integer.toString(attempt_counter));
-                            if(attempt_counter == 0){
-                                login_btn.setEnabled(false);
-                            }
-                        }
+                        mastername = username.getText().toString();
+                        masterpassword = password.getText().toString();
 
+                        new FetchSQLgetpassword().execute(mastername, masterpassword);
                     }
                 }
         );
